a0f434f8af1c05e97b2cebe7cb6495d167bdb82b
Vasivvv <tu4ev0bpe@code.edu.az>
2022-11-02 00:49:18 +0400

    Arrays tasks done

---
+++ array 1,2.cs
@@ -0,0 +1,25 @@
+﻿#region task 1
+//int[] arr = { 32, 13, 65, 74, 8, 4, 5 };
+//int min = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < min)
+//    {
+//        min = arr[i];
+//    }
+
+//}
+//Console.WriteLine(min);
+#endregion
+#region task 2
+//int[] arr = { 111, 212, 313, 4, 5, 616, 7, 8, 919 };
+//int count = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 0 && arr[i] < 10)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+#endregion
